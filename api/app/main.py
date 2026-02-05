695d50d9052112e44006dbc6da71865acdc7ae0a
yodhasu <yodhasu@gmail.com>
2026-02-05 22:40:18 +0700

    edit be main

--- api/app/main.py
+++ api/app/main.py
@@ -33,7 +33,7 @@ def create_application() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.CORS_ORIGINS,
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
